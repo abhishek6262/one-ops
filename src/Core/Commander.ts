37dabb07e1b6b79ef7e8aa91b59c30c1c51c86cd
Abhishek Prakash2 <prakashabhishek6262@gmail.com>
2022-10-02 03:53:45 +0530

    Execute command

--- src/Core/Commander.ts
+++ src/Core/Commander.ts
@@ -3,37 +3,40 @@ import { CommandExecutor } from '../Contracts/CommandExecutor';
 import { Command as ICommand } from './Command';
 
 export class Commander implements CommandExecutor {
-  private executor: Command;
+  private app: Command;
 
   constructor() {
-    this.executor = new Command();
+    this.app = new Command();
   }
 
   execute(): void {
-    this.executor.parse();
+    this.app.parse();
   }
 
-  registerCommands(...commands: (new () => ICommand)[]) {
+  registerCommands(...commands: (new () => ICommand)[]): CommandExecutor {
     for (const command of commands) {
       const obj = new command();
-      this.executor.command(obj.name).description(obj.description);
+      this.app
+        .command(obj.name)
+        .description(obj.description)
+        .action(() => obj.execute());
     }
 
     return this;
   }
 
   setName(name: string): CommandExecutor {
-    this.executor.name(name);
+    this.app.name(name);
     return this;
   }
 
   setDescription(description: string): CommandExecutor {
-    this.executor.description(description);
+    this.app.description(description);
     return this;
   }
 
   setVersion(version: string): CommandExecutor {
-    this.executor.version(version);
+    this.app.version(version);
     return this;
   }
 }
