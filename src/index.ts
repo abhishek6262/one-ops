c1392af1afdeb4a7c78ee4a4d29c42fa09f79770
Abhishek Prakash <prakashabhishek6262@gmail.com>
2022-10-01 08:43:52 +0000

    Reset project

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 #! /usr/bin/env node
 
-import "reflect-metadata";
 import { Command } from 'commander';
 import { argv } from 'process';
-import { container } from "./Framework/Container";
-import * as commands from "./Commands";
 
 function main() {
-    const program = new Command();
+  const program = new Command();
 
-    program.version('0.1.0').description('One-Ops is a cli tool for managing your devops needs');
-
-    for (const className of Object.values(commands)) {
-        const commandInstance = container.resolve(className);
-        const command = program
-            .command(commandInstance.getName())
-            .description(commandInstance.getDescription());
-
-        for (const arg of commandInstance.getArgs())
-            command.argument(arg.getFormattedName(), arg.getDescription());
-
-        command.action(commandInstance.execute);
-    }
-
-    program.parse(argv);
+  program.parse(argv);
 }
 
 main();
